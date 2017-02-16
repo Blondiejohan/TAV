8adad8dec62a9d5cc2ae523b2c43246446ab3768
NNXN <NNXN@10.0.128.3>
2017-02-16 13:04:51 +0200

    mock bef supervision

--- src/test/MockMovementController.java
+++ src/test/MockMovementController.java
@@ -21,21 +21,21 @@ import main.WrongInputException;
 @RunWith (MockitoJUnitRunner.class)
 public class MockMovementController implements MovementControllerInterface {
 	
-
-	MovementControllerInterface move;
+	
+	Position move;
 	
 	@Mock
-	private Position position;
+	Position position;
 	
 	@Mock
-	private ParkingSpot bestSpot;
+	ParkingSpot bestSpot;
 	
 	@Mock
     Car testCar;
-    
+	
     @Before
-    public void create() throws WrongInputException{
-    	Position position1 = new Position(0,0,false);
+    public void setUp() throws WrongInputException{
+    	initMocks(this);
     	int[] tmpArr3 = {1,1,1,1,1};
 		int[][] tmpArr1 = new int[501][5];
 		int[][] tmpArr2 = new int[501][5];
@@ -43,18 +43,32 @@ public class MockMovementController implements MovementControllerInterface {
 			tmpArr1[i] = tmpArr3;
 			tmpArr2[i] = tmpArr3;
 		}
+		
 		testCar = new Car(0,false,tmpArr1, tmpArr2);
-		//move.setLocation(0);
-    	move = mock(MovementControllerInterface.class); //create MoveController mock object
+		position = new Position(0,false);
+		bestSpot = new ParkingSpot(0,0);
+		
+    	move = mock(Position.class); //create MoveController mock object
+    	when(move.getLocation()).thenReturn(0);
     	
+    	//when
     	//verify
+    	System.out.println(position.getLocation());
     }
 
-    @Test
-    public void testMovementForward(){
-        //assertEquals(0,move.getLocation());
-        
-        //verify(move, times(1)).getLocation();
+    private void initMocks(MockMovementController mockMovementController) throws WrongInputException {
+    	//mockMovementController.accelerate();
+    	mockMovementController.position.getLocation();
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Test
+    public void testMovementForward() throws WrongInputException{
+       
+		assertSame(move.getLocation(),0);
+        //verify(move).;
+        verify(move, times(5)).getLocation();
     }
     
     
